0fdbef3d60a8ce0d95c11d34f9a8618469b2d372
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-11-25 01:07:04 -0300

    Inicializar repositorio

---
+++ src/test/java/com/frm/prog3/parcial2/Parcial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frm.prog3.parcial2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
